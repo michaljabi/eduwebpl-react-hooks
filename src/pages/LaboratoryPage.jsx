23f8318be7edc2b212fba7a881ee8757f26cc0dc
Jabi <michal.jabi@gmail.com>
2024-08-21 16:41:48 +0200

    lesson9(LaboratoryPage): prepare toggle

--- src/pages/LaboratoryPage.jsx
+++ src/pages/LaboratoryPage.jsx
@@ -1,22 +1,29 @@
 import { PageLayout } from "../layouts/PageLayout.jsx"
 import { Notification } from "../components/ui/Notification.jsx"
-import { Inception } from "../components/lab/Inception.jsx"
+// import { Inception } from "../components/lab/Inception.jsx"
 import { Counter } from "../components/lab/Counter.jsx"
+import { Button } from "../components/ui/Button.jsx"
+import { useState } from "react"
 // import { CounterClass } from "../components/lab/CounterClass.jsx"
 
 export function LaboratoryPage() {
   console.log("🧪 %cLaboratoryPage", `color: teal`, "fired")
+
+  const [isShown, setIsShown] = useState(true)
 
   return (
     <PageLayout title="Laboratory">
       <Notification className="m-6 lg:w-[60%] lg:mx-auto" type="warn">
         This page is here <em> only </em> for our Experiments...
       </Notification>
+      <Button as="secondary" onClick={() => setIsShown(!isShown)}>
+        Toggle counter
+      </Button>
       <section className="my-8 flex justify-center">
-        <Counter />
+        {isShown && <Counter />}
         {/*<CounterClass />*/}
       </section>
-      <Inception />
+      {/*<Inception />*/}
     </PageLayout>
   )
 }
