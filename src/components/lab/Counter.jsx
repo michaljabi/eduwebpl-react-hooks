a1b7889f1ca85f5566fecabbea1574f6b0328343
Jabi <michal.jabi@gmail.com>
2024-08-21 12:12:46 +0200

    lesson7(Counter)[TASK]: solved!

--- src/components/lab/Counter.jsx
+++ src/components/lab/Counter.jsx
@@ -6,12 +6,14 @@ import { Notification } from "../ui/Notification.jsx"
 export function Counter() {
   const [count, setCount] = useState(1)
 
+  const isToHigh = count >= 5
+
   return (
     <div className="rounded p-6 border border-slate-400 text-center">
       <div className="m-6 p-3 text-2xl bg-gray-200 dark:bg-slate-800 rounded w-1/2 mx-auto">
         {count}
       </div>
-      <Notification type="danger">Level too high!</Notification>
+      {isToHigh && <Notification type="danger">Level too high!</Notification>}
       <Button
         className="m-2"
         as="secondary"
@@ -19,15 +21,20 @@ export function Counter() {
       >
         <MinusIcon color="red" /> minus
       </Button>
-      <Button onClick={() => setCount(count + 1)}>
+      <Button
+        onClick={() => {
+          if (!isToHigh) {
+            setCount(count + 1)
+          }
+        }}
+      >
         <PlusIcon color="lightgreen" /> plus
       </Button>
-      {/*<Button as="secondary" onClick={addTwo}>
-        <PlusIcon color="lightgreen" /> +2
-        </Button> */}
-      <Button as="secondary">
-        <TrashIcon /> reset
-      </Button>
+      {isToHigh && (
+        <Button as="secondary" onClick={() => setCount(1)}>
+          <TrashIcon /> reset
+        </Button>
+      )}
     </div>
   )
 }
