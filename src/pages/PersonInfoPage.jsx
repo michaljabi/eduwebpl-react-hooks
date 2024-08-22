b228d67962bbfc68597090899f9770cb5b831533
Jabi <michal.jabi@gmail.com>
2024-08-22 10:26:26 +0200

    lesson12(PersonInfoPage): UI to show full info about person.

--- src/pages/PersonInfoPage.jsx
+++ src/pages/PersonInfoPage.jsx
@@ -20,6 +20,13 @@ export function PersonInfoPage() {
       {isLoading && (
         <Notification>Loading data about {personName}...</Notification>
       )}
+      <section className="m-3 p-3 border space-y-4 rounded">
+        <h3 className="ml-4">name</h3>
+        <strong className="ml-4">email</strong>
+        <div className="m-4 p-2 bg-gray-200 dark:bg-slate-700 dark:text-gray-100 rounded">
+          bio
+        </div>
+      </section>
     </PageLayout>
   )
 }
